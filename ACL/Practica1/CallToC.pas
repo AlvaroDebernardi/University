f827b2f91aa7da8ab5b5ac7b083925aa0bc14d88
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-08-22 11:51:31 -0300

    Practice 1 for ACL and DB

---
+++ ACL/Practica1/CallToC.pas
@@ -0,0 +1,12 @@
+Program CallToC;
+{$L inc.o}
+
+Var x:integer; external name 'y';
+
+Procedure inc_x; external name 'inc_y';
+
+begin { programa principal }
+    x := 1;
+    inc_x;
+    writeln('x=',x)
+end.
