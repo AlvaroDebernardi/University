371bba7a9c4d1a989ea4fb82e278e1e11817c491
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-08-25 22:20:04 -0300

    Implement: best spare/strike bonus

--- IngenieriaSoftware/Practico/1-tdd/bowling/src/main/java/Player.java
+++ IngenieriaSoftware/Practico/1-tdd/bowling/src/main/java/Player.java
@@ -1,17 +1,16 @@
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Player {
     private int score;
-    private int spareBonus;
-    private int strikeBonus;
+    private LinkedList<Integer> bonus;
     private int pinsLeft;
     private int framePlayed;
     private int turnOnFrame;
 
     public Player() {
         score = 0;
-        spareBonus = 0;
-        strikeBonus = 0;
+        bonus = new LinkedList<>();
         pinsLeft = 10;
         turnOnFrame = 1;
         framePlayed = 0;
@@ -20,15 +19,14 @@ public class Player {
     public void roll(int pins) {
         if (pinsLeft - pins < 0)
             throw new IllegalArgumentException(
-                    pins + "-Pins down " + pinsLeft + "-Left");
+                "\u001B[31m" + pinsLeft + "-Left " + pins + "-Down\u001B[0m");
 
         applyPoints(pins);
-
 
         if (turnOnFrame == 1) {
             if (pins == 10) {
                 pinsLeft = 10;
-                strikeBonus += 2;
+                bonus.add(2);
                 framePlayed++;
             } else {
                 pinsLeft -= pins;
@@ -36,7 +34,7 @@ public class Player {
             }
         } else {
             if(pinsLeft - pins == 0)
-                spareBonus++;
+                bonus.add(1);
 
             pinsLeft = 10;
             framePlayed++;
@@ -47,25 +45,14 @@ public class Player {
     private void applyPoints(int pinsDown) {
         score += pinsDown;
 
-        if (spareBonus == 1) {
+        for (Integer b : bonus)
             score += pinsDown;
-            spareBonus = 0;
-        }
-        if (strikeBonus >= 1) {
-            score += pinsDown;
-            strikeBonus -= 1;
-        }
+
+        bonus.removeIf(b -> b == 1);
+        bonus.replaceAll(b -> b - 1);
     }
 
     public int score() {
         return score;
-    }
-
-    public int spareBonus() {
-        return spareBonus;
-    }
-
-    public int strikeBonus() {
-        return strikeBonus;
     }
 }
--- IngenieriaSoftware/Practico/1-tdd/bowling/src/test/java/TestBowling.java
+++ IngenieriaSoftware/Practico/1-tdd/bowling/src/test/java/TestBowling.java
@@ -19,6 +19,7 @@ public class TestBowling {
         Player player = new Player();
         int pindDown = 11;
 
+        player.roll(pindDown);
         assertThrows(IllegalArgumentException.class, () -> {
             player.roll(pindDown);
         });
@@ -37,7 +38,6 @@ public class TestBowling {
         });
     }
 
-
     @Test
     void play2FrameTest() {
         Player player = new Player();
@@ -50,7 +50,89 @@ public class TestBowling {
         player.roll(pindDown3);
 
         assertEquals(30,player.score());
-        assertEquals(player.spareBonus(),0);
-        assertEquals(player.strikeBonus(),2);
+    }
+
+    @Test
+    void gameTest() {
+        Player player = new Player();
+        int pindDown1 = 8;
+        int pindDown2 = 2;
+        int pindDown3 = 10;
+        int pindDown4 = 5;
+        int pindDown5 = 4;
+        int pindDown6 = 10;
+        int pindDown7 = 1;
+        int pindDown8 = 0;
+        int pindDown9 = 6;
+        int pindDown10 = 4;
+        int pindDown11 = 5;
+        int pindDown12 = 3;
+        int pindDown13 = 9;
+        int pindDown14 = 1;
+        int pindDown15 = 10;
+        int pindDown16 = 3;
+        int pindDown17 = 4;
+
+        player.roll(pindDown1);
+        player.roll(pindDown2);
+        player.roll(pindDown3);
+        player.roll(pindDown4);
+        player.roll(pindDown5);
+        player.roll(pindDown6);
+        player.roll(pindDown7);
+        player.roll(pindDown8);
+        player.roll(pindDown9);
+        player.roll(pindDown10);
+        player.roll(pindDown11);
+        player.roll(pindDown12);
+        player.roll(pindDown13);
+        player.roll(pindDown14);
+        player.roll(pindDown15);
+        player.roll(pindDown16);
+        player.roll(pindDown17);
+
+        assertEquals(127,player.score());
+    }
+
+    @Test
+    void game2Test() {
+        Player player = new Player();
+        int pindDown1 = 8;
+        int pindDown2 = 2;
+        int pindDown3 = 10;
+        int pindDown4 = 10;
+        //int pindDown5 = 4;
+        int pindDown6 = 10;
+        int pindDown7 = 1;
+        int pindDown8 = 0;
+        int pindDown9 = 6;
+        int pindDown10 = 4;
+        int pindDown11 = 5;
+        int pindDown12 = 3;
+        int pindDown13 = 9;
+        int pindDown14 = 1;
+        int pindDown15 = 10;
+        int pindDown16 = 3;
+        int pindDown17 = 4;
+
+        player.roll(pindDown1);
+        player.roll(pindDown2);
+        player.roll(pindDown3);
+        player.roll(pindDown4);
+        //player.roll(pindDown5);
+        player.roll(pindDown6);
+        player.roll(pindDown7);
+        player.roll(pindDown8);
+        player.roll(pindDown9);
+        player.roll(pindDown10);
+        player.roll(pindDown11);
+        player.roll(pindDown12);
+        player.roll(pindDown13);
+        player.roll(pindDown14);
+        player.roll(pindDown15);
+        player.roll(pindDown16);
+        player.roll(pindDown17);
+
+        assertEquals(150,player.score());
     }
 }
