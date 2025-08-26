4e16e286d6ce7083e7ba24f5fbe604f103fc0c77
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-08-25 21:02:00 -0300

    Implement: bowling start

--- IngenieriaSoftware/Practico/1-tdd/bowling/src/main/java/Player.java
+++ IngenieriaSoftware/Practico/1-tdd/bowling/src/main/java/Player.java
@@ -1,15 +1,64 @@
+import java.util.Stack;
+
 public class Player {
+    private int score;
+    private int spareBonus;
+    private int strikeBonus;
+    private int pinsLeft;
+    private int framePlayed;
+    private int turnOnFrame;
 
-    public void play() {
-
+    public Player() {
+        score = 0;
+        spareBonus = 0;
+        strikeBonus = 0;
+        pinsLeft = 10;
+        framePlayed = 0;
+        turnOnFrame = 1;
     }
 
     public void roll(int pins) {
+        if (pinsLeft - pins < 0)
+            throw new IllegalArgumentException("the number of pins are grater that are left");
+        score += pins;
 
+        if (spareBonus == 1) {
+            score += 2 * pins;
+            spareBonus = 0;
+        }
+        if (strikeBonus >= 1) {
+            score += 2 * pins;
+            strikeBonus -= 1;
+        }
+
+        if (turnOnFrame == 1) {
+            if (pins == 10) {
+                pinsLeft = 10;
+                strikeBonus += 2;
+                framePlayed++;
+            } else {
+                pinsLeft -= pins;
+                turnOnFrame = 2;
+            }
+        } else {
+            if(pinsLeft == 0) {
+                spareBonus++;
+            }
+            pinsLeft = 10;
+            framePlayed++;
+            turnOnFrame = 1;
+        }
     }
 
     public int score() {
-        int score;
         return score;
+    }
+
+    public int spareBonus() {
+        return spareBonus;
+    }
+
+    public int strikeBonus() {
+        return strikeBonus;
     }
 }
--- IngenieriaSoftware/Practico/1-tdd/bowling/src/test/java/TestBowling.java
+++ IngenieriaSoftware/Practico/1-tdd/bowling/src/test/java/TestBowling.java
@@ -3,10 +3,56 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class TestBowling {
-    Player player = new Player();
 
     @Test
-    void game() {
-        int[] pindDown = new int[10];
+    void playFrameTest() {
+        Player player = new Player();
+        int pindDown = 8;
+
+        player.roll(pindDown);
+
+        assertEquals(player.score(),8);
+    }
+
+    @Test
+    void InvalidRollTest() {
+        Player player = new Player();
+        int pindDown = 11;
+
+        player.roll(pindDown);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            player.roll(pindDown);
+        });
+    }
+
+    @Test
+    void secondRollInvalidTest() {
+        Player player = new Player();
+        int pindDown1 = 5;
+        int pindDown2 = 6;
+
+        player.roll(pindDown1);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            player.roll(pindDown2);
+        });
+    }
+
+
+    @Test
+    void play2FrameTest() {
+        Player player = new Player();
+        int pindDown1 = 8;
+        int pindDown2 = 2;
+        int pindDown3 = 10;
+
+        player.roll(pindDown1);
+        player.roll(pindDown2);
+        player.roll(pindDown3);
+
+        assertEquals(30,player.score());
+        assertEquals(player.spareBonus(),0);
+        assertEquals(player.strikeBonus(),1);
     }
 }
