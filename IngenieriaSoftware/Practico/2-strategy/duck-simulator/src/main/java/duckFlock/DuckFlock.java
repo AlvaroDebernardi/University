f9d47b533b4bff4eb4843f9993def99fef104dea
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-09-05 00:02:13 -0300

    Add: duck flock class

---
+++ IngenieriaSoftware/Practico/2-strategy/duck-simulator/src/main/java/duckFlock/DuckFlock.java
@@ -0,0 +1,13 @@
+package duckFlock;
+
+import java.util.*;
+import duck.*;
+
+public abstract class DuckFlock {
+    private int cant;
+    private List<Duck> ducks;
+    private FlyFlockBehavior flyBehavior;
+    private QuackFlockBehavior quackBehavior;
+
+    
+}
